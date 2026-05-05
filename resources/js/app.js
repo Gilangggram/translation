63703a5f1881de5d5bc5ca06e4994a9041504a1e
XZenma <154389234+XZenma@users.noreply.github.com>
2026-05-06 01:39:38 +0700

    chore: add chart.js and livewire dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import 'flowbite';
+import Chart from 'chart.js/auto';
 
 import { initPasswordToggle } from './modules/password-toggle';
+window.Chart = Chart;
 
 document.addEventListener('DOMContentLoaded', () => {
     initPasswordToggle();
